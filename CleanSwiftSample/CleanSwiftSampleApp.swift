e21451c940a92d31858deefcf6b354b950b38e01
이상훈 <sanghoon.lee@ikoob.com>
2023-04-03 09:20:24 +0900

    Initial Commit

---
+++ CleanSwiftSample/CleanSwiftSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanSwiftSampleApp.swift
+//  CleanSwiftSample
+//
+//  Created by sanghoon.lee on 2023/04/03.
+//
+
+import SwiftUI
+
+@main
+struct CleanSwiftSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
